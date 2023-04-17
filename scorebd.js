cc84eb1c4c93f1774602d8ec4e616b0dc7b1b315
Diamondos7 <ododosamson3@gmail.com>
2023-04-17 14:41:41 +0100

    negatives

--- scorebd.js
+++ scorebd.js
@@ -1,62 +1,11 @@
-// let tm1 = document.getElementById("score-home")
-// let tm2 = document.getElementById("score-guest")
-// let wn = document.getElementById("won")
-// let count = 0
-// let countt = 0
+//starting the game
 
-// function add1() {
-// 	count += 1
-// 	tm1.innerText = count
-// 	console.log(tm1)
-// }
-
-// function add2() {
-// 	count += 2
-// 	tm1.innerText = count
-// 	console.log(tm1)
-// }
-
-// function add3() {
-// 	count += 3
-// 	tm1.innerText = count
-// 	console.log(tm1)
-// }
+var c= "14"
+var u= "465"
+if (c=>u) {alert("Let the game begin!")}
 
 
-// function add(){
-// 	countt += 1
-// 	tm2.innerText = countt
-// 	console.log(countt)
-// }
 
-// function addd(){
-// 	countt += 2
-// 	tm2.innerText = countt
-// 	console.log(countt)
-// }
-
-// function adddd(){
-// 	countt += 3
-// 	tm2.innerText = countt
-// 	console.log(countt)
-// }
-
-// function win(){
-// let number1Element = document.getElementById("score-home"); // Get the element with ID "number1"
-// let number2Element = document.getElementById("sscore-guest"); // Get the element with ID "number2"
-
-// let number1 = parseInt(number1Element.innerHTML); // Convert innerHTML of "number1" to integer
-// let number2 = parseInt(number2Element.innerHTML); // Convert innerHTML of "number2" to integer
-
-// 	if (number1 > number2) {
-// 		console.log("The greatest number is: " + number1); // Output: "The greatest number is: 1234"
-// 	} else if (number2 > number1) {
-// 		console.log("The greatest number is: " + number2); // Output: "The greatest number is: 5678"
-// 	} else {
-// 		console.log("The numbers are equal"); // Output: "The numbers are equal"
-// 	}
-
-// }
 
 let tm1 = document.getElementById("score-home")
 let tm2 = document.getElementById("score-guest")
@@ -100,6 +49,34 @@ function adddd(){
 	console.log(countt)
 }
 
+
+function sub1() {
+	count -= 1
+	tm1.innerText = count
+	console.log(tm1)
+}
+
+function sub2() {
+	count -= 2
+	tm1.innerText = count
+	console.log(tm1)
+}
+
+
+function sub(){
+	countt -= 1
+	tm2.innerText = countt
+	console.log(countt)
+}
+
+function subb(){
+	countt -= 2
+	tm2.innerText = countt
+	console.log(countt)
+}
+
+
+
 function win(){
 	let number1Element = tm1; // Get the element with ID "score-home"
 	let number2Element = tm2; // Get the element with ID "score-guest"
@@ -112,7 +89,7 @@ function win(){
         wn.innerHTML = message;
         console.log(message); // Output: "The greatest number is: 1234"
     } else if (number2 > number1) {
-        let message = "GuestTWON!";
+        let message = "GuestWon!";
         wn.innerHTML = message;
         console.log(message); // Output: "The greatest number is: 5678"
     } else {
@@ -120,11 +97,10 @@ function win(){
         wn.innerHTML = message;
         console.log(message); // Output: "The numbers are equal"
     }
+
+	
 }
 
-var c= "14"
-var u= "465"
-if (c=>u) {alert("Let the game begin!")}
 
 
 // timer starts
@@ -166,40 +142,3 @@ stopButton.addEventListener("click", stopTimer);
 
 //timer ends
 
-
-// let tm1 = document.getElementById("score-home")
-// let tm2 = document.getElementById("score-guest")
-// let count = 0
-// let countt = 0
-// function add1() {
-// 	count += 1
-// 	ome.inneText = count
-// 	console.log(ome)
-// }
-
-// function add2() {
-// 	count += 2
-// 	home.inneText = count
-// 	console.log(home)
-// }
-
-// function add3() {
-// 	count += 3
-// 	home.inneText = count
-// 	console.log(home)
-// }
-// function add1() {
-// 	countt += 1
-// 	guest.inneText = count
-// 	console.log(guest)
-// }
-// function add2() {
-// 	countt += 2
-// 	guest.inneText = count
-// 	console.log(guest)
-// }
-// function add3() {
-// 	countt += 3
-// 	guest.inneText = count
-// 	console.log(guest)
-// }
