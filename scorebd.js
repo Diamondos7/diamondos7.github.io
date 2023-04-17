7d2619263ca598f21147300fde42f6ca5712df24
Diamondos7 <ododosamson3@gmail.com>
2023-04-17 15:57:15 +0100

    reset done

--- scorebd.js
+++ scorebd.js
@@ -2,104 +2,225 @@
 
 var c= "14"
 var u= "465"
-if (c=>u) {alert("Let the game begin!")}
+if (c=>u) {alert("🎇🎇🎇Let the game begin🎇🎇🎇!")}
 
+//22222222222222222222222222222222222222222222222222222222222222
 
+let tm1 = document.getElementById("score-home");
+let tm2 = document.getElementById("score-guest");
+let wn = document.getElementById("won");
+let count = parseInt(localStorage.getItem("score-home")) || 0;
+let countt = parseInt(localStorage.getItem("score-guest")) || 0;
 
-
-let tm1 = document.getElementById("score-home")
-let tm2 = document.getElementById("score-guest")
-let wn = document.getElementById("won")
-let count = 0
-let countt = 0
+tm1.innerText = count;
+tm2.innerText = countt;
 
 function add1() {
-	count += 1
-	tm1.innerText = count
-	console.log(tm1)
+	count += 1;
+	tm1.innerText = count;
+	localStorage.setItem("score-home", count);
+	console.log(tm1);
 }
 
 function add2() {
-	count += 2
-	tm1.innerText = count
-	console.log(tm1)
+	count += 2;
+	tm1.innerText = count;
+	localStorage.setItem("score-home", count);
+	console.log(tm1);
 }
 
 function add3() {
-	count += 3
-	tm1.innerText = count
-	console.log(tm1)
+	count += 3;
+	tm1.innerText = count;
+	localStorage.setItem("score-home", count);
+	console.log(tm1);
 }
 
-function add(){
-	countt += 1
-	tm2.innerText = countt
-	console.log(countt)
+function add() {
+	countt += 1;
+	tm2.innerText = countt;
+	localStorage.setItem("score-guest", countt);
+	console.log(countt);
 }
 
-function addd(){
-	countt += 2
-	tm2.innerText = countt
-	console.log(countt)
+function addd() {
+	countt += 2;
+	tm2.innerText = countt;
+	localStorage.setItem("score-guest", countt);
+	console.log(countt);
 }
 
-function adddd(){
-	countt += 3
-	tm2.innerText = countt
-	console.log(countt)
+function adddd() {
+	countt += 3;
+	tm2.innerText = countt;
+	localStorage.setItem("score-guest", countt);
+	console.log(countt);
 }
+
 
 
 function sub1() {
 	count -= 1
 	tm1.innerText = count
-	console.log(tm1)
+	localStorage.setItem("score-home", count);
+	console.log(tm1);
 }
 
 function sub2() {
 	count -= 2
 	tm1.innerText = count
-	console.log(tm1)
+	localStorage.setItem("score-home", count);
+	console.log(tm1);
 }
 
 
 function sub(){
 	countt -= 1
 	tm2.innerText = countt
-	console.log(countt)
+	localStorage.setItem("score-guest", countt);
+	console.log(countt);
 }
 
 function subb(){
 	countt -= 2
 	tm2.innerText = countt
-	console.log(countt)
+	localStorage.setItem("score-guest", countt);
+	console.log(countt);
 }
 
 
+function win() {
+	let number1Element = document.getElementById("score-home");
+	let number2Element = document.getElementById("score-guest");
 
-function win(){
-	let number1Element = tm1; // Get the element with ID "score-home"
-	let number2Element = tm2; // Get the element with ID "score-guest"
-
-	let number1 = parseInt(number1Element.innerHTML); // Convert innerHTML of "score-home" to integer
-	let number2 = parseInt(number2Element.innerHTML); // Convert innerHTML of "score-guest" to integer
+	let number1 = parseInt(number1Element.innerHTML);
+	let number2 = parseInt(number2Element.innerHTML);
 
 	if (number1 > number2) {
-        let message = "HomeWon!";
-        wn.innerHTML = message;
-        console.log(message); // Output: "The greatest number is: 1234"
-    } else if (number2 > number1) {
-        let message = "GuestWon!";
-        wn.innerHTML = message;
-        console.log(message); // Output: "The greatest number is: 5678"
-    } else {
-        let message = "DRAW!!!";
-        wn.innerHTML = message;
-        console.log(message); // Output: "The numbers are equal"
-    }
+		let message = "HomeWon!" + number1;
+		wn.innerHTML = message;
+		console.log(message);
+		alert(" 🎇🎇Congratulations Home team you won with a total of: "  + number1  + "points 🎇🎇")
+	} else if (number2 > number1) {
+		let message = "Guest Won!" + number2;
+		wn.innerHTML = message;
+		console.log(message);
+		alert(" 🎇🎇Congratulations Guest team you won with a total of: "  + number2  + "points 🎇🎇")
+	} else {
+		let message = "Draw!!!";
+		wn.innerHTML = message;
+		console.log(message);
+		alert(" It's a tie!!!")
+	}
+}
 
+function reset() {
+	count =parseInt("0"); 
+	tm1.innerText = count;
+	countt = parseInt("0");
+	tm2.innerText = countt;
+	
+
+	localStorage.clear()
 	
 }
+
+
+
+// //2222222222222222222222222222222222
+// let tm1 = document.getElementById("score-home")
+// let tm2 = document.getElementById("score-guest")
+// let wn = document.getElementById("won")
+// let count = 0
+// let countt = 0
+
+// function add1() {
+// 	count += 1
+// 	tm1.innerText = count
+// 	console.log(tm1)
+// }
+
+// function add2() {
+// 	count += 2
+// 	tm1.innerText = count
+// 	console.log(tm1)
+// }
+
+// function add3() {
+// 	count += 3
+// 	tm1.innerText = count
+// 	console.log(tm1)
+// }
+
+// function add(){
+// 	countt += 1
+// 	tm2.innerText = countt
+// 	console.log(countt)
+// }
+
+// function addd(){
+// 	countt += 2
+// 	tm2.innerText = countt
+// 	console.log(countt)
+// }
+
+// function adddd(){
+// 	countt += 3
+// 	tm2.innerText = countt
+// 	console.log(countt)
+// }
+
+
+// function sub1() {
+// 	count -= 1
+// 	tm1.innerText = count
+// 	console.log(tm1)
+// }
+
+// function sub2() {
+// 	count -= 2
+// 	tm1.innerText = count
+// 	console.log(tm1)
+// }
+
+
+// function sub(){
+// 	countt -= 1
+// 	tm2.innerText = countt
+// 	console.log(countt)
+// }
+
+// function subb(){
+// 	countt -= 2
+// 	tm2.innerText = countt
+// 	console.log(countt)
+// }
+
+
+
+// function win(){
+// 	let number1Element = tm1; // Get the element with ID "score-home"
+// 	let number2Element = tm2; // Get the element with ID "score-guest"
+
+// 	let number1 = parseInt(number1Element.innerHTML); // Convert innerHTML of "score-home" to integer
+// 	let number2 = parseInt(number2Element.innerHTML); // Convert innerHTML of "score-guest" to integer
+
+// 	if (number1 > number2) {
+//         let message = "HomeWon!";
+//         wn.innerHTML = message;
+//         console.log(message); // Output: "The greatest number is: 1234"
+//     } else if (number2 > number1) {
+//         let message = "GuestWon!";
+//         wn.innerHTML = message;
+//         console.log(message); // Output: "The greatest number is: 5678"
+//     } else {
+//         let message = "DRAW!!!";
+//         wn.innerHTML = message;
+//         console.log(message); // Output: "The numbers are equal"
+//     }
+
+	
+// }
 
 
 
